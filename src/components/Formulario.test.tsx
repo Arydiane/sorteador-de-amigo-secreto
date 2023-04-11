9324ab6b5bd4d53ed62cca2d41e6c9b5516b60eb
Arydiane <arydianejardim@gmail.com>
2023-04-11 17:48:17 -0300

    instala dependências sass e css modules

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import React from "react";
+import Formulario from "./Formulario";
 
 //Jest
 test('quando o input está vazio novos participantes não podem ser adicionados', () => {
