cb60f33b07497ff6b8126a245426cad9c0a89f9f
Arydiane <arydianejardim@gmail.com>
2023-04-11 20:35:45 -0300

    refatora hook adicionar participante não permite a adição de nomes duplicados na lista

--- src/state/hook/useAdicionarParticipante.ts
+++ src/state/hook/useAdicionarParticipante.ts
@@ -1,9 +1,16 @@
-import { useSetRecoilState } from "recoil"
-import { listaDeParticipantesState } from "../atom"
+import { useRecoilValue, useSetRecoilState } from "recoil"
+import { erroState, listaDeParticipantesState } from "../atom"
 
 export const useAdicionarParticpante = () => {
     const setLista = useSetRecoilState(listaDeParticipantesState)
+    const lista = useRecoilValue(listaDeParticipantesState)
+    const setErro = useSetRecoilState(erroState)
+
     return (nomeParticipante: string) => {
+        if (lista.includes(nomeParticipante)) {
+            setErro('Nomes duplicados não são permitidos!')
+            return 
+        }
         return setLista(listaAtual => [...listaAtual, nomeParticipante])
     }
 }
