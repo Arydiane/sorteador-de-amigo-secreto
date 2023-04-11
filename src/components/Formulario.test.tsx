617531681892492031d177d583b1ff13ca7e3f93
Arydiane <arydianejardim@gmail.com>
2023-04-11 20:10:25 -0300

    cria teste para verificar formulario ao adicionar participante

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,20 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Formulario from "./Formulario";
+import { RecoilRoot } from "recoil";
 
-//Jest
 test('quando o input está vazio novos participantes não podem ser adicionados', () => {
-    
    render(<Formulario />)
-
     //encontrar no dom o input
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-
     //encontrar o botão
     const botao = screen.getByRole('button')
-
     //garantir que o input esteja no documento
     expect(input).toBeInTheDocument()
-
     //garantir que o botão esteja desabilitado
     expect(botao).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>)
+    //encontrar no dom o input
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    //encontrar o botão
+    const botao = screen.getByRole('button')
+    //inserir um valor no input
+    fireEvent.change(input, {
+        target: {
+            value: 'Arydiane'
+        }
+    })
+    //clicar no botão de submeter
+    fireEvent.click(botao)
+    //garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus()
+    //garantir que o input não tenha um valor
+    expect(input).toHaveValue("")
 })
