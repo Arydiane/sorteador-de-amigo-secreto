1c8650b0192b1ecc962b1f4461bee4adfb125ba2
Arydiane <arydianejardim@gmail.com>
2023-04-11 20:52:58 -0300

    refatora hook adicionar participante para exibir a mensagem de erro por  apenas alguns segundos

--- src/state/hook/useAdicionarParticipante.ts
+++ src/state/hook/useAdicionarParticipante.ts
@@ -9,6 +9,9 @@ export const useAdicionarParticpante = () => {
     return (nomeParticipante: string) => {
         if (lista.includes(nomeParticipante)) {
             setErro('Nomes duplicados não são permitidos!')
+            setTimeout(()=> {
+                setErro("")
+            }, 5000)
             return 
         }
         return setLista(listaAtual => [...listaAtual, nomeParticipante])
