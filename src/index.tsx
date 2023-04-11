7a1f3e45617107ca679ec5e6b234e80498de8855
Arydiane <arydianejardim@gmail.com>
2023-04-11 18:40:51 -0300

    adiciona estilização básica

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Cabecalho from './components/Cabecalho';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Cabecalho />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
